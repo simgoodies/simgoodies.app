c20979eb078b917539ac59504315a2e1a74b681b
Roel Gonzalez <roelgonzalez94@gmail.com>
2019-03-12 03:42:55 +0100

    add cookie consent notification

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,7 +5,8 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
-- Nothing yet...
+### Added
+Cookie consent notification
 
 ## [1.1.0](https://github.com/vatsimgoodies/vatgoodies.com/compare/v1.0.2..v1.1.0)
 ### Added
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -25,6 +25,25 @@
             </script>
     @endif
 
+    <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.css" />
+    <script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.js"></script>
+    <script>
+        window.addEventListener("load", function(){
+            window.cookieconsent.initialise({
+                "palette": {
+                    "popup": {
+                        "background": "#edeff5",
+                        "text": "#838391"
+                    },
+                    "button": {
+                        "background": "#4b81e8"
+                    }
+                },
+                "theme": "classic",
+                "position": "bottom-right"
+            })});
+    </script>
+
 <!-- Bootstrap -->
     <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
 
