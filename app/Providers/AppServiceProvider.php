4fc88fb6ca2ddd7e3d0f30dd42a49fdf4fa9c3a3
Roël Gonzalez <roelgonzalez94@gmail.com>
2020-08-04 14:42:40 +0200

    fix issue so bugsnag only reports issues in staging or production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if ($this->app->environment() == ('production' || 'staging')) {
+        if (in_array($this->app->environment(), ['production', 'staging'])) {
             $this->app->alias('bugsnag.multi', \Illuminate\Contracts\Logging\Log::class);
             $this->app->alias('bugsnag.multi', \Psr\Log\LoggerInterface::class);
         }
